da3d93854dc5ab656a87247d8c9bdfd109dcb5c9
Vlad <vlads1976@gmail.com>
2023-03-25 13:58:49 +0100

    created new func get_context_data for calculate difference between last and prev records meters in database.

--- add_meters/views.py
+++ add_meters/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views import View
 from django.views.generic import ListView, UpdateView, DetailView, TemplateView, FormView
+from datetime import datetime
+import pytz
 
 from .forms import AddMeterForm
 from .models import AddMeterData
@@ -27,6 +29,29 @@ class ProfileListView(ListView):
         qs_last = AddMeterData.objects.filter(user=self.request.user).order_by('-created')[:1]
         qs_prev = AddMeterData.objects.filter(user=self.request.user).order_by('-created')[1:2]
         return list(qs_last) + list(qs_prev)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Get the last two records in the database
+        records = AddMeterData.objects.filter(user=self.request.user).order_by('-created')[:2]
+        # If there are at least two records, calculate the difference
+        if len(records) == 2:
+            last_record = records[0]
+            prev_record = records[1]
+            date_now = datetime.now(tz=pytz.timezone("Europe/Amsterdam"))
+            diff_meter_1 = last_record.meter_1 - prev_record.meter_1
+            diff_meter_2 = last_record.meter_2 - prev_record.meter_2
+            diff_meter_3 = last_record.meter_3 - prev_record.meter_3
+            diff_meter_4 = last_record.meter_4 - prev_record.meter_4
+            diff_meter_5 = last_record.meter_5 - prev_record.meter_5
+            context['diff_meter_1'] = diff_meter_1
+            context['diff_meter_2'] = diff_meter_2
+            context['diff_meter_3'] = diff_meter_3
+            context['diff_meter_4'] = diff_meter_4
+            context['diff_meter_5'] = diff_meter_5
+            context['date_now'] = date_now
+
+        return context
 
 
 class MeterFormView(View):
@@ -50,7 +75,7 @@ class MeterUpdateView(UpdateView):
     model = AddMeterData
     fields = ['meter_1', 'meter_2', 'meter_3', 'meter_4', 'meter_5']
     template_name = 'add_meters/update.html'
-    success_url = reverse_lazy('meters:main')
+    success_url = reverse_lazy('meters:profile')
 
     def get_object(self, queryset=None):
         try:
